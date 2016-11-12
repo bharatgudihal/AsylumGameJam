c7a58b3d03b00063b5789d17385ba5fc017fd50f
Bharat Gudihal <bharat.gudi@gmail.com>
2016-11-12 03:26:21 -0700

    PHASE1!!!!

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EventManager : MonoBehaviour {
+	public delegate void CameraShake (float intensity, float decay);
+	public static event CameraShake shakeCamera;
+	public delegate void WriteText (List<TextElement> dialogue);
+	public static event WriteText textWriter;
+	public delegate void ChangePhase ();
+	public static event ChangePhase phaseChanger;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public static void CameraShaker(float intensity, float decay){
+		shakeCamera(intensity, decay);
+	}
+
+	public static void CallTextWriter(List<TextElement> dialogue){
+		textWriter (dialogue);
+	}
+
+	public static void CallPhaseChanger(){
+		phaseChanger ();
+	}
+}
