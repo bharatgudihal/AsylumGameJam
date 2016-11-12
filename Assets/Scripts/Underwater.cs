cea6b4729e8f4f4349c492b01c672d6a8ef47120
JPsLucky13 <u1070819@ad.utah.edu>
2016-11-12 01:20:45 -0700

    Added underwater asset

---
+++ Assets/Scripts/Underwater.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Underwater : MonoBehaviour {
+
+	bool fog = true;
+	Color fogColor = new Color (0.22f,0.65f,0.77f,0.5f);
+	float fogDensity = 0.002f;
+	Material skybox;
+
+	// Use this for initialization
+	void Start () {
+//
+
+		//RenderSettings.skybox = skybox;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RenderSettings.fogColor = fogColor;
+		RenderSettings.fogDensity = fogDensity;
+	}
+}
