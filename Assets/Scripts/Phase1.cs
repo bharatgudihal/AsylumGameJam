a7ba39a07f8f88c670c8fcb4011efa4c637e1ac3
JPsLucky13 <u1070819@ad.utah.edu>
2016-11-12 12:14:28 -0700

    Fixed fog

--- Assets/Scripts/Phase1.cs
+++ Assets/Scripts/Phase1.cs
@@ -6,9 +6,13 @@ public class Phase1 : MonoBehaviour {
 
     public float upperLimit;
 	public float lowerLimit;
+	private float timer = 0.0f;
+	private float surfaceWaitTime = 25.0f;
+	private float phase1WaitTime = 15.0f;
+	public float changeSpeed;
     public GameObject BathySphere;
-	public float climbSpeed;
-	public float descendSpeed;
+	public Color highestSurface;
+	public Color phase1Color;
 	public bool upperLimitReached;
 	public bool startDescent;
 	List<TextElement> dialogue;
@@ -17,8 +21,11 @@ public class Phase1 : MonoBehaviour {
 	private string test3 = "The Vader is in the water for you !!!";
 	private string test4 = "Maybe you should consider the razor";
 
+
     // Use this for initialization
     public void Start () {
+		RenderSettings.fogColor = phase1Color;
+
 		dialogue = new List<TextElement> ();
 
 		TextElement element1 = new TextElement(test1,0.1f);
@@ -35,31 +42,37 @@ public class Phase1 : MonoBehaviour {
 	
 	// Update is called once per frame
 	public void Update () {		
+
+		timer += Time.deltaTime;
 		if (!upperLimitReached) {
-			float initPos = BathySphere.transform.position.y;
-			float finalPos = Mathf.Lerp (initPos, upperLimit, climbSpeed);
-			BathySphere.transform.position = new Vector3(BathySphere.transform.position.x,finalPos,BathySphere.transform.position.z);
-			if (finalPos >= upperLimit-1) {
-				upperLimitReached = true;
+			Color currentColor = Color.Lerp (RenderSettings.fogColor, highestSurface, changeSpeed);
+			RenderSettings.fogColor = currentColor;
+
+
+			if (timer > surfaceWaitTime) {
 				StartCoroutine (WaitForOneSecond ());
+				timer = 0.0f;
+				upperLimitReached = true;
 			}
 		}
 		if (startDescent) {
-			float initPos = BathySphere.transform.position.y;
-			float finalPos = Mathf.Lerp (initPos, lowerLimit, descendSpeed);
-			BathySphere.transform.position = new Vector3(BathySphere.transform.position.x,finalPos,BathySphere.transform.position.z);
-			if (finalPos <= lowerLimit+1) {
-				startDescent = false;
+			Color currentColor = Color.Lerp (RenderSettings.fogColor,phase1Color,changeSpeed * 1.5f);
+			RenderSettings.fogColor = currentColor;
+
+			if (timer > phase1WaitTime) {
 				EventManager.CallPhaseChanger ();
+				startDescent = false;
 			}
 		}
 	}
 
 	IEnumerator  WaitForOneSecond(){
+		
 		yield return new WaitForSeconds (1.0f);
 		EventManager.CameraShaker (0.03f, 0.0005f);
 		startDescent = true;
 		yield return new WaitForSeconds (1.0f);
 		EventManager.CameraShaker (0.03f, 0.00009f);
+
 	}
 }
---
+++ Assets/Scripts/Phase2.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Phase2 : MonoBehaviour {
+
+    public float upperLimit;
+	public float lowerLimit;
+    public GameObject BathySphere;
+	public float climbSpeed;
+	public float descendSpeed;
+	public bool upperLimitReached;
+	public bool startDescent;
+	List<TextElement> dialogue;
+	private string test1 = "Come in, is there a problem?";
+	private string test2 = "Try pulling down the top right lever to raise yourself!";
+
+    // Use this for initialization
+    public void Start () {
+		dialogue = new List<TextElement> ();
+
+		TextElement element1 = new TextElement(test1,0.1f);
+		TextElement element2 = new TextElement(test2,0.1f);
+
+		dialogue.Add (element1);
+		dialogue.Add (element2);
+		EventManager.CallTextWriter (dialogue);
+	}
+	
+	// Update is called once per frame
+	public void Update () {		
+		if (!upperLimitReached) {
+			float initPos = BathySphere.transform.position.y;
+			float finalPos = Mathf.Lerp (initPos, upperLimit, climbSpeed);
+			BathySphere.transform.position = new Vector3(BathySphere.transform.position.x,finalPos,BathySphere.transform.position.z);
+			if (finalPos >= upperLimit-1) {
+				upperLimitReached = true;
+				StartCoroutine (WaitForOneSecond ());
+			}
+		}
+		if (startDescent) {
+			float initPos = BathySphere.transform.position.y;
+			float finalPos = Mathf.Lerp (initPos, lowerLimit, descendSpeed);
+			BathySphere.transform.position = new Vector3(BathySphere.transform.position.x,finalPos,BathySphere.transform.position.z);
+			if (finalPos <= lowerLimit+1) {
+				startDescent = false;
+				EventManager.CallPhaseChanger ();
+			}
+		}
+	}
+
+	IEnumerator  WaitForOneSecond(){
+		yield return new WaitForSeconds (1.0f);
+		EventManager.CameraShaker (0.03f, 0.0005f);
+		startDescent = true;
+		yield return new WaitForSeconds (1.0f);
+		EventManager.CameraShaker (0.03f, 0.00009f);
+	}
+}
